4ae5166381e3cb32880744b622fc06361807c14c
Erik Everson <Erik.Everson@ditto.live>
2024-03-01 10:46:22 -0700

    Added attachments with DQL querys

--- index.ts
+++ index.ts
@@ -1,82 +1,191 @@
+import { init, Ditto, Document, QueryResultItem } from "@dittolive/ditto";
+import * as readline from "readline/promises";
+import { stdin as input, stdout as output } from "node:process";
 
-import { init, Ditto, Document } from '@dittolive/ditto'
-import * as readline from 'readline/promises'
-import { stdin as input, stdout as output } from 'node:process';
+let ditto;
+let tasks: Document[] = [];
+// Attachments dont seem to show up as part of a document. Using Query item instead.
+let attachments: QueryResultItem[] = [];
+let queryResult;
 
-let ditto
-let subscription
-let liveQuery
-let tasks: Document[] = []
-
-async function main () {
-  await init()
+async function main() {
+  await init();
 
   ditto = new Ditto({
-    type: 'onlinePlayground',
-    appID: 'YOUR_APP_ID',
-    token: 'YOUR_TOKEN'
-  })
-  ditto.startSync()
+    type: "onlinePlayground",
+    appID: "Your_App_ID",
+    token: "Your_App_Token",
+  });
+  ditto.startSync();
 
-  subscription = ditto.store.collection("tasks").find("isDeleted == false").subscribe()
-  liveQuery = ditto.store.collection("tasks").find("isDeleted == false").observeLocal((docs, event) => {
-    tasks = docs
-  })
-  let isAskedToExit = false
-  
+  ditto.sync.registerSubscription(
+    `SELECT * FROM tasks WHERE isDeleted = false`,
+  );
+
+  ditto.sync.registerSubscription(
+    `SELECT * FROM COLLECTION attachments (my_attachment ATTACHMENT) WHERE isDeleted = false`,
+  );
+
+  ditto.store.registerObserver(
+    `SELECT * FROM tasks WHERE isDeleted = false AND isCompleted = false`,
+    (result) => {
+      tasks = result.items.map((doc) => {
+        return doc.value;
+      });
+    },
+  );
+
+  ditto.store.registerObserver(
+    "SELECT * FROM COLLECTION attachments (my_attachment ATTACHMENT) WHERE isDeleted = false AND isCompleted = false",
+    (result) => {
+      attachments = result.items;
+      // Attachments dont seem to show up as part of a document
+      //   .map((doc) => {
+      //   return doc.value;
+      // });
+    },
+  );
+
+  let isAskedToExit = false;
+
   console.log("************* Commands *************");
   console.log("--insert my new task");
   console.log("   Inserts a task");
-  console.log("   Example: \"--insert Get Milk\"");
+  console.log('   Example: "--insert Get Milk"');
   console.log("--toggle myTaskTd");
   console.log("   Toggles the isComplete property to the opposite value");
-  console.log("   Example: \"--toggle 1234abc\"");
+  console.log('   Example: "--toggle 1234abc"');
   console.log("--delete myTaskTd");
   console.log("   Deletes a task");
-  console.log("   Example: \"--delete 1234abc\"");
+  console.log('   Example: "--delete 1234abc"');
   console.log("--list");
   console.log("   List the current tasks");
+  console.log("--add path_to_attachment");
+  console.log("   adds an attachment from the path to the file.");
+  console.log('   Example: "--insert Get Milk"');
+  console.log("--attachments");
+  console.log("   List the current attachments");
   console.log("--exit");
   console.log("   Exits the program");
   console.log("************* Commands *************");
 
   const rl = readline.createInterface({ input, output });
+
   while (!isAskedToExit) {
+    let answer = await rl.question("Your command:");
 
-      let answer = await rl.question('Your command:')
-      if (answer.startsWith("--insert")) {
-        let body = answer.replace("--insert ", "")
-        ditto.store.collection("tasks").upsert({
-          body,
+    if (answer.startsWith("--insert")) {
+      let body = answer.replace("--insert ", "");
+      const newTask = { body, isDeleted: false, isCompleted: false };
+
+      await ditto.store.execute(`INSERT INTO tasks DOCUMENTS (:newTask)`, {
+        newTask,
+      });
+    }
+
+    if (answer.startsWith("--add")) {
+      let path = answer.replace("--add ", "");
+      const metadata = { name: "image.png" };
+
+      // Copy the file into Ditto's store and create an attachment object.
+      const myAttachment = await ditto.store.newAttachment(path, metadata);
+      console.log(myAttachment);
+
+      if (path.includes("123")) {
+        const newDQLAttachment = {
+          path,
+          _id: "123",
           isDeleted: false,
-          isCompleted: false
-        })
-      }
-      if (answer.startsWith("--toggle")) {
-        let id = answer.replace("--toggle ", "")
-        ditto.store.collection("tasks")
-        .findByID(id).update((doc) => {
-          let isCompleted = doc.value.isCompleted
-          doc.at("isCompleted").set(!isCompleted)
-        })
-      }
-      if (answer.startsWith("--list")) {
-        console.log(tasks.map((task) => task.value))
-      }
-      if (answer.startsWith("--delete")) {
-        let id = answer.replace("--delete ", "")
-        ditto.store.collection("tasks")
-        .findByID(id).update((doc) => {
-          doc.at("isDeleted").set(true)
-        })
-      }
-      if (answer.startsWith("--exit")) {
-        ditto.stopSync()
-        process.exit()
-        
-      }
-  }
+          isCompleted: false,
+          my_attachment: myAttachment,
+        };
 
+        // Insert the document into the collection, marking `my_attachment` as an
+        // attachment field.
+        await ditto.store.execute(
+          `INSERT INTO COLLECTION attachments (my_attachment ATTACHMENT)
+             DOCUMENTS (:newDQLAttachment)`,
+          { newDQLAttachment },
+        );
+      } else {
+        const newDQLAttachment = {
+          path,
+          isDeleted: false,
+          isCompleted: false,
+          my_attachment: myAttachment,
+        };
+
+        // Insert the document into the collection, marking `my_attachment` as an
+        // attachment field.
+        await ditto.store.execute(
+          `INSERT INTO COLLECTION attachments (my_attachment ATTACHMENT)
+             DOCUMENTS (:newDQLAttachment)`,
+          { newDQLAttachment },
+        );
+      }
+    }
+
+    if (answer.startsWith("--toggle")) {
+      let id = answer.replace("--toggle ", "");
+      queryResult = await ditto.store.execute(
+        `SELECT * FROM tasks WHERE _id = :id`,
+        { id },
+      );
+      let newValue = !queryResult.items.map((item) => {
+        return item.value.isCompleted;
+      })[0];
+      await ditto.store.execute(
+        `UPDATE tasks SET isCompleted = :newValue  WHERE _id = :id`,
+        { id, newValue },
+      );
+    }
+
+    if (answer.startsWith("--list")) {
+      console.log(tasks);
+    }
+
+    if (answer.startsWith("--attachments")) {
+      //console.log(attachments);
+
+      // Fetch the attachment token from a document in the store
+      // the '123' syntax is very important single quote
+      const result = await ditto.store.execute(
+        `
+           SELECT *
+           FROM COLLECTION attachments (my_attachment ATTACHMENT) WHERE _id = '123'`,
+      );
+      console.log(result);
+
+      const token = result.items[0].value.my_attachment;
+      const attachment = await ditto.store.fetchAttachment(token);
+      const attachmentData = await attachment.data();
+
+      console.log("First attachments data:");
+      console.log(attachmentData);
+
+      // Get attchment from the observer.
+
+      const token_Array = attachments[0].value.my_attachment;
+      const attachment_Array = await ditto.store.fetchAttachment(token_Array);
+      const attachmentDataFromArray = await attachment_Array.data();
+
+      console.log("Second attachments data:");
+      console.log(attachmentDataFromArray);
+    }
+
+    if (answer.startsWith("--delete")) {
+      let id = answer.replace("--delete ", "");
+      await ditto.store.execute(
+        `UPDATE tasks SET isDeleted = true WHERE _id = :id`,
+        { id },
+      );
+    }
+
+    if (answer.startsWith("--exit")) {
+      ditto.stopSync();
+      process.exit();
+    }
+  }
 }
 
-main()
+main();
